f498b04c404492d0f62534bcaf574acb59937c22
Anmol Sarin <anmolsarin251003@gmail.com>
2026-02-23 23:58:58 +0530

    2nd

--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -19,7 +19,7 @@ class LoginPage extends StatelessWidget{
               Column(
                 children: [
                 Spacer(),
-                Text("Hello, !",
+                Text("Hello, User !",
                   style:TextStyle(
                   fontSize:30,
                   fontWeight:FontWeight.bold,
