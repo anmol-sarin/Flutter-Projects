d1361fef4a0f1a0c8d40527fb35d061c96fe49e9
Anmol Sarin <anmolsarin251003@gmail.com>
2026-03-04 23:57:18 +0530

    new commit2

--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -107,9 +107,9 @@ class LoginPage extends StatelessWidget{
           ),
           ElevatedButton(
             style:ElevatedButton.styleFrom(
-              // foregroundColor: Colors.black,
-              // backgroundColor:Colors.white,
-              // shape:RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(50)))
+              foregroundColor: Colors.black,
+              backgroundColor:Colors.white,
+              shape:RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(50)))
             ),
             onPressed: (){
             print("clicked google !!");
